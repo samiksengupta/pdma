cc86ad758b665c8e96e95d5aaf6f4c60cf3a57bc
samiksengupta <samiksengupta@hotmail.com>
2023-09-21 22:19:23 +0530

    enabled dark mode for quasar

--- src/quasar-user-options.js
+++ src/quasar-user-options.js
@@ -5,7 +5,9 @@ import '@quasar/extras/material-icons/material-icons.css'
 
 // To be used on app.use(Quasar, { ... })
 export default {
-  config: {},
-  plugins: {
-  }
+    config: {
+        dark: true
+    },
+    plugins: {
+    }
 }
